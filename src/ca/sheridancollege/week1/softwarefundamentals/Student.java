cf7b506802f8230cce1014ad3fe3da27df1531b6
Ann <rachelpa@sheridancollege.ca>
2020-06-02 18:55:01 -0400

    Second Commit

--- src/ca/sheridancollege/week1/softwarefundamentals/Student.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/Student.java
@@ -7,53 +7,11 @@ package ca.sheridancollege.week1.softwarefundamentals;
 
 /**
  *
- * @author asidhu annrp
+ * @author annrp
  */
 public class Student {
-    
+   
     private String name;
-    
-    private String email;
-    
-    private int yob;
-
-    /**
-     * Get the value of yob
-     *
-     * @return the value of yob
-     */
-    public int getYob() {
-        return yob;
-    }
-
-    /**
-     * Set the value of yob
-     *
-     * @param yob new value of yob
-     */
-    public void setYob(int yob) {
-        this.yob = yob;
-    }
-
-
-    /**
-     * Get the value of email
-     *
-     * @return the value of email
-     */
-    public String getEmail() {
-        return email;
-    }
-
-    /**
-     * Set the value of email
-     *
-     * @param email new value of email
-     */
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
 
     /**
      * Get the value of name
@@ -72,7 +30,5 @@ public class Student {
     public void setName(String name) {
         this.name = name;
     }
-
-}
  
-   
+}
